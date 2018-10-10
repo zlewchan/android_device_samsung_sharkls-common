e23ebb591aa8ec417b84b6bb3fa191802cd1d1d5
djeman <djemouli@gmail.com>
2018-10-10 20:40:22 +0200

    modules: script refactor
    Change-Id: I6ebf68a561049eb9ee8d42b75f54848f8f739631

--- modules.mk
+++ modules.mk
@@ -1,7 +1,15 @@
 
-SPRD_MODULES_CCOMPILE := $(PWD)/prebuilts/gcc/$(HOST_PREBUILT_TAG)/arm/arm-eabi-4.8/bin/arm-eabi-
+GPU_DRIVER_PATH := vendor/sprd/modules/libgpu/gpu/utgard
+WLAN_DRIVER_PATH := vendor/sprd/wcn/wifi/sc2331/6.0
+
+ifeq ($(TARGET_BUILD_VARIANT),eng)
+MBUILD_VAR := debug
+else
+MBUILD_VAR := release
+endif
 
 SPRD_MODULES:
+	mkdir -p $(PRODUCT_OUT)/system/lib/modules
 	$(hide) if grep -q '^CONFIG_MODULES=y' $(KERNEL_CONFIG); then \
 			echo "Installing Kernel Modules"; \
 			$(MAKE) $(MAKE_FLAGS) -C $(KERNEL_SRC) O=$(KERNEL_OUT) ARCH=$(KERNEL_ARCH) $(KERNEL_CROSS_COMPILE) INSTALL_MOD_PATH=../../$(KERNEL_MODULES_INSTALL) modules_install && \
@@ -10,17 +18,17 @@ SPRD_MODULES:
 			for f in $$(find $$mpath/kernel -type f -name '*.ko'); do \
 				$(KERNEL_TOOLCHAIN_PATH)strip --strip-unneeded $$f; \
 				mv $$f $(KERNEL_MODULES_OUT); \
+				mfile=$$(basename $$f); \
+				ln -sf /lib/modules/$$mfile $(PRODUCT_OUT)/system/lib/modules/$$mfile; \
 			done && \
 			rm -rf $$mpath; \
 		fi
-	make -C vendor/sprd/modules/libgpu/gpu/utgard/ MALI_PLATFORM=sc8830 BUILD=debug KDIR=$(KERNEL_OUT) CROSS_COMPILE=$(SPRD_MODULES_CCOMPILE) clean
-	make -C vendor/sprd/modules/libgpu/gpu/utgard/ MALI_PLATFORM=sc8830 BUILD=debug KDIR=$(KERNEL_OUT) CROSS_COMPILE=$(SPRD_MODULES_CCOMPILE)
-	mv vendor/sprd/modules/libgpu/gpu/utgard/mali.ko $(KERNEL_MODULES_OUT)
-	make -C vendor/sprd/wcn/wifi/sc2331/6.0/ SPRDWL_PLATFORM=sc8830 USING_PP_CORE=2 BUILD=debug KDIR=$(KERNEL_OUT) CROSS_COMPILE=$(SPRD_MODULES_CCOMPILE) clean
-	make -C vendor/sprd/wcn/wifi/sc2331/6.0/ SPRDWL_PLATFORM=sc8830 USING_PP_CORE=2 BUILD=debug KDIR=$(KERNEL_OUT) CROSS_COMPILE=$(SPRD_MODULES_CCOMPILE)
-	mv vendor/sprd/wcn/wifi/sc2331/6.0/sprdwl.ko $(KERNEL_MODULES_OUT)
-	mkdir -p $(PRODUCT_OUT)/system/lib/modules
-	ln -sf /lib/modules/autotst.ko $(PRODUCT_OUT)/system/lib/modules/autotst.ko
+	make -C $(GPU_DRIVER_PATH) MALI_PLATFORM=sc8830 BUILD=$(MBUILD_VAR) KDIR=$(KERNEL_OUT) CROSS_COMPILE=$(KERNEL_CROSS_COMPILE) clean
+	make -C $(GPU_DRIVER_PATH) MALI_PLATFORM=sc8830 BUILD=$(MBUILD_VAR) KDIR=$(KERNEL_OUT) CROSS_COMPILE=$(KERNEL_CROSS_COMPILE)
+	make -C $(WLAN_DRIVER_PATH) SPRDWL_PLATFORM=sc8830 USING_PP_CORE=2 BUILD=$(MBUILD_VAR) KDIR=$(KERNEL_OUT) CROSS_COMPILE=$(KERNEL_CROSS_COMPILE) clean
+	make -C $(WLAN_DRIVER_PATH) SPRDWL_PLATFORM=sc8830 USING_PP_CORE=2 BUILD=$(MBUILD_VAR) KDIR=$(KERNEL_OUT) CROSS_COMPILE=$(KERNEL_CROSS_COMPILE)
+	mv $(GPU_DRIVER_PATH)/mali.ko $(KERNEL_MODULES_OUT)
+	mv $(WLAN_DRIVER_PATH)/sprdwl.ko $(KERNEL_MODULES_OUT)
 	ln -sf /lib/modules/mali.ko $(PRODUCT_OUT)/system/lib/modules/mali.ko
-	ln -sf /lib/modules/mmc_test.ko $(PRODUCT_OUT)/system/lib/modules/mmc_test.ko
 	ln -sf /lib/modules/sprdwl.ko $(PRODUCT_OUT)/system/lib/modules/sprdwl.ko
+
